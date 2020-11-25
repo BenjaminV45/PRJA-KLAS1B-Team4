605170151609a1d791cc54fb86ab0d069d7f2b76
BenjaminV45 <45715664+BenjaminV45@users.noreply.github.com>
2020-11-25 14:17:37 +0100

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,48 @@
+//git test test
+
+function setup() {
+  createCanvas(800, 600);
+  menu = new Menu();
+}
+
+function draw() {
+  background(34, 117, 188);
+  menu.menu()
+}
+
+class Menu {
+  
+  constructor() {
+    
+    
+  }
+  
+  menu(){
+    
+    this.background(255)
+    this.button(75, this.page1);
+    this.button(75 + 105 + 75);
+    this.button(75 + 105 + 75 + 105 + 75);
+    this.button(75 + 105 + 75 + 105 + 75 + 105 + 75);
+    
+  }
+  
+  button(x) {
+    var y = 472.5
+    stroke(color(0, 170, 255));
+    strokeWeight(6);
+    fill(0);
+    rect(x, y, 105, 105);
+    
+  }
+  
+  
+  background(color){
+    
+    noStroke();
+    fill(color)
+    rect(0, 450, 800, 150)
+    
+  }
+  
+}
